60b9f5ce37c89f9a88eee720757580ebd431ba35
Adam Lipson <alipson98@gmail.com>
2020-01-22 18:03:05 -0500

    fixed full adder

--- lab1/Lab1/Lab1.srcs/sources_1/new/full_adder.vhd
+++ lab1/Lab1/Lab1.srcs/sources_1/new/full_adder.vhd
@@ -32,16 +32,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity full_adder is
-    Port ( a : in STD_LOGIC_VECTOR (3 downto 0);
-           b : in STD_LOGIC_VECTOR (3 downto 0);
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
            Cin : in STD_LOGIC;
            Cout : out STD_LOGIC;
-           sum : out STD_LOGIC_VECTOR (3 downto 0));
+           sum : out STD_LOGIC);
 end full_adder;
 
 architecture arch of full_adder is
 
 begin
-
+    sum <= (a xor b) xor Cin;
+    Cout <= (a and b) or (Cin and (a xor b));     
 
 end arch;
